9bafcf5efc83e9e73ed0fe3566eb8246df29a3e5
Tara Vancil <tbvanc@gmail.com>
2016-07-29 01:11:41 -0500

    Set up watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
+var browserSync = require('browser-sync');
 
 gulp.task('minify-html', function() {
   return gulp.src('html/*')
@@ -11,14 +12,26 @@ gulp.task('minify-html', function() {
       minifyJS: true,
       removeComments: true
     }))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
 });
 
 gulp.task('build-css', function() {
   return gulp.src('css/*')
     .pipe(cssmin())
     .pipe(concat('style.min.css'))
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.reload({stream: true}));
 })
 
-gulp.task('build',  ['minify-html', 'build-css']);
+gulp.task('build', ['minify-html', 'build-css']);
+
+gulp.task('server', function() {
+  browserSync({
+    server: 'build'
+  });
+});
+
+gulp.task('watch', ['build', 'server'], function() {
+  gulp.watch('css/*', ['build-css']);
+  gulp.watch('html/*', ['minify-html', browserSync.reload()]);
+});
