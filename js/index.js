ebc89f1fe5eaeff3c88667336686b426ec05f58f
Tara Vancil <tbvanc@gmail.com>
2016-08-06 22:59:26 -0500

    Add app entry

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>Testing, testing...</div>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById('nippler-container'))
