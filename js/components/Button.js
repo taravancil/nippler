4d56b1218a18991d36c315c7849e5eb7ae782f4a
Tara Vancil <tbvanc@gmail.com>
2016-08-07 02:29:28 -0500

    Add base styles

--- js/components/Button.js
+++ js/components/Button.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
+baseStyles = {
+  border-radius: '2px',
+  outline: 'none',
+  border: 'none',
+  background: 'none',
+  height: '40px',
+  padding: '0 1em',
+  margin: '.5em',
+  fontSize: '.8em'
+}
+
 const Button = (props) =>
   <button
     id={props.id}
     type='button'
     aria-label={props.alt}
+    style={baseStyles}
     className={'btn btn--' + props.type}
     onClick={props.handler}
     disabled={props.disabled}>
