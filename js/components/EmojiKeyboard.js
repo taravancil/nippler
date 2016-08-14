f80b846e73d6e415088ea065d6457a20eafb78aa
Tara Vancil <tbvanc@gmail.com>
2016-08-13 22:27:24 -0500

    Add base styles

--- js/components/EmojiKeyboard.js
+++ js/components/EmojiKeyboard.js
@@ -2,6 +2,30 @@ import Emoji from './Emoji.js'
 
 import React from 'react'
 
+const styles = {
+  base: {
+    width: '350'
+  },
+  categories: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    padding: '.4em .3em .1em .3em'
+  },
+  category: {
+    width: '25px',
+    fill: 'blue'
+  },
+  chars: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexFlow: 'column wrap',
+    overflowX: 'auto',
+    background: 'white',
+    height: '135',
+    padding: '.4em 0'
+  }
+}
+
 const EmojiKeyboard = React.createClass({
   getInitialState () {
     return {
@@ -30,26 +54,29 @@ const EmojiKeyboard = React.createClass({
   },
   render () {
     return (
-      <div>
-        <div>
+      <div style={styles.base}>
+        <div style={styles.categories}>
           {this.state.categories.map((category, i) =>
-            <a href={'#emoji-' + category.title} key={i}>
-              <svg viewBox='0 0 24 24'>
-                <title>{category.title}</title>
-                <path d={category.icon}></path>
-              </svg>
+            <a
+              href={'#emoji-' + category.title}
+              key={i}
+              style={styles.category}>
+                <svg viewBox='0 0 24 24'>
+                  <title>{category.title}</title>
+                  <path d={category.icon}></path>
+                </svg>
             </a>
           )}
         </div>
-        <div>
-          {this.state.chars.map((emoji, i) => {
+        <div style={styles.chars}>
+          {this.state.chars.map((emoji, i) =>
             <Emoji
               key={i}
               char={emoji.char}
               options={emoji.options}
               category={emoji.category}
               handler={this.props.handler} />
-          })}
+          )}
         </div>
       </div>
     )
