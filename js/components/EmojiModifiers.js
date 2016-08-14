aff09aa37f0b34deb602786f42b1097a5550f0da
Tara Vancil <tbvanc@gmail.com>
2016-08-14 15:08:57 -0500

    Only style visibility inline

--- js/components/EmojiModifiers.js
+++ js/components/EmojiModifiers.js
@@ -1,15 +1,25 @@
 import React from 'react'
 
+const styles = {
+  hidden: {
+    zIndex: '-2'
+  },
+  visible: {
+    zIndex: '2'
+  }
+}
+
 const EmojiModifiers = (props) =>
-  <div style={{zIndex: props.zIndex}} onMouseLeave={props.hideModifiers}>
-    {['🏻', '🏼', '🏽', '🏾', '🏿'].map((mod, j) =>
-      <span
-        key={j}
-        onMouseOver={props.selectModifier}
-        onClick={props.hideModifiers}>
-          {props.char + mod}
-      </span>
-    )}
+  <div style={props.visible ? styles.visible : styles.hidden}
+    onMouseLeave={props.hideModifiers}>
+      {['🏻', '🏼', '🏽', '🏾', '🏿'].map((mod, j) =>
+        <span
+          key={j}
+          onMouseOver={props.selectModifier}
+          onClick={props.hideModifiers}>
+            {props.char + mod}
+        </span>
+      )}
   </div>
 
 export default EmojiModifiers
