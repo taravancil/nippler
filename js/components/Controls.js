91b929b1fd93b802c5c5a8e2d1ec94296df389e1
Tara Vancil <tbvanc@gmail.com>
2016-08-15 00:39:38 -0500

    Add styles

--- js/components/Controls.js
+++ js/components/Controls.js
@@ -3,10 +3,27 @@ import EmojiKeyboard from './EmojiKeyboard'
 import NippleGallery from './NippleGallery'
 import Tabs from './Tabs'
 
+const styles = {
+  base: {
+    width: '100%',
+    padding: '0 1em'
+  },
+  heading: {
+    display: 'block',
+    width: '60px',
+    fontWeight: 'bold',
+    background: '#fff',
+    color: 'blue',
+    marginBottom: '20px',
+    border: '1px solid blue',
+    boxShadow: '3px 3px 0 blue',
+    transform: 'rotate(-1deg)'
+  }
+}
 const Controls = ({disabled, handleEmoji, handleNipple, updateNippleRadius}) =>
-  <div className='controls'>
+  <div className='controls' style={styles.base}>
     <p className='controls__size'>
-      <label htmlFor='nipple-size'>Nipple size</label>
+      <label htmlFor='nipple-size' style={styles.heading}>size</label>
       <input
         name='nipple-size'
         type='range'
@@ -16,6 +33,7 @@ const Controls = ({disabled, handleEmoji, handleNipple, updateNippleRadius}) =>
         disabled={disabled}
         onChange={updateNippleRadius} />
     </p>
+    <p style={styles.heading}>style</p>
     <Tabs
       tabs={['nipples', 'emoji']}
       content={[
