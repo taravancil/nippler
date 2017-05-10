77bf58a9006f17546899dd987ed522c1f366dc47
Tara Vancil <tbvanc@gmail.com>
2017-05-10 14:10:41 -0500

    Remove service worker

--- js/index.js
+++ js/index.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom'
 
 import Editor from './components/Editor'
 
-navigator.serviceWorker.register('service-worker.js', {
-  scope: '.'
-})
-
 ReactDOM.render(<Editor />, document.getElementById('nippler-container'))
