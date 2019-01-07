4a0e33394aa3e45a8bda9300507b432d952bdf3a
Tara Vancil <tbvanc@gmail.com>
2019-01-07 00:37:42 -0600

    Update nipple gallery

--- src/js/components/NippleGallery.js
+++ src/js/components/NippleGallery.js
@@ -3,18 +3,23 @@ import React from "react";
 const nipples = new Array(10);
 nipples.fill(1);
 
-const NippleGallery = ({ handler }) => (
-  <div className="nipple-gallery flex">
+const NippleGallery = ({ handler, currentNippleIdx }) => (
+  <div className="nipple-gallery grid">
     {nipples.map((nipple, i) => (
       <label key={i} htmlFor={"nipple-" + i} className="nipple">
         <input
           name="nipple-type"
           id={"nipple-" + i}
           type="radio"
-          onClick={handler}
+          onChange={handler}
+          checked={i === currentNippleIdx}
           value={"/assets/images/n" + (i + 1) + ".png"}
         />
-        <img src={"/assets/images/n" + (i + 1) + ".png"} />
+        <img
+          width="55"
+          height="55"
+          src={"/assets/images/n" + (i + 1) + ".png"}
+        />
       </label>
     ))}
   </div>
